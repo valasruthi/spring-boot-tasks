7e1a3a9531f7c0351ef48064b011e0cb274662ca
valasruthi <valasruthi4@mail.com>
2019-08-02 13:06:41 +0530

    added swagger

--- src/main/java/com/stackroute/muzixtrack/MuzixTrackApplication.java
+++ src/main/java/com/stackroute/muzixtrack/MuzixTrackApplication.java
@@ -2,8 +2,10 @@ package com.stackroute.muzixtrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MuzixTrackApplication {
 
   public static void main(String[] args) {
